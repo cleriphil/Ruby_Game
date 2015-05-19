d1305274a2cf2a4a1b8949474eb822b37a2fca59
Kim Dang <kimdangg@gmail.com>
2015-05-19 13:49:04 -0700

    Add page 9 and 10 routes

--- app.rb
+++ app.rb
@@ -1,3 +1,44 @@
 require('bundler/setup')
 Bundler.require(:default, :production)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+get('/page9') do
+  @error = false
+  erb(:page9)
+end
+
+post('/page9') do
+  item_id = params.fetch('item')
+  item = Item.find(item_id)
+  if item.description == 'torch'
+    erb(:page10)
+  else
+    @error = true
+    erb(:page9)
+  end
+end
+
+get('/page10') do
+  erb(:page10)
+end
