df2ee335bbc1a00518ae6fce8b23a4b68b09ae31
ian bevel & andrew lee <student@epicodus.com>
2015-05-19 12:16:55 -0700

    add error page, complete 3rd page

--- app.rb
+++ app.rb
@@ -9,3 +9,24 @@ end
 get('/page1') do
   erb(:page1)
 end
+
+get('/page2') do
+  answer1 = params.fetch('first_spot')
+  answer2 = params.fetch('second_spot')
+  if answer1 == "get" && answer2 == "cave_entrance"
+    erb(:page2)
+  else
+    erb(:error)
+  end
+end
+
+get('/page3') do
+  answer1 = params.fetch('first_spot')
+  answer2 = params.fetch('second_spot')
+  if answer1 == "new" && answer2 == "save"
+    # @items = Item.all()
+    erb(:page3)
+  else
+    erb(:error)
+  end
+end
