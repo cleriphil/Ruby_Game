50f983b5f7bff1533f2f647e8de86a3f97a57f08
ian bevel & andrew lee <student@epicodus.com>
2015-05-19 11:52:45 -0700

    add content to first page as well as add additional 8 pages to project

--- app.rb
+++ app.rb
@@ -1,3 +1,11 @@
 require('bundler/setup')
 Bundler.require(:default, :production)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/page1') do
+  erb(:page1)
+end
