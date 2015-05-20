69022cc22f933e4f8f3649988146546c27faf734
andrew lee & ian bevel <student@epicodus.com>
2015-05-20 11:25:51 -0700

    update app.rb and erb files to be more user friendly and remain consistent among branches

--- app.rb
+++ app.rb
@@ -56,78 +56,89 @@ end
 post('/page3') do
   answer1 = params.fetch('first_spot')
   answer2 = params.fetch('second_spot')
-  if answer1 == "new" && answer2 == "save"
+  answer3 = params.fetch('third_spot')
+  @items = Item.all()
+  if answer1 == "6" && answer2 == "push" && answer3 == "join"
     erb(:page4)
   else
     @error = true
-    @items = Item.all()
     erb(:page3)
   end
 end
 
 get('/page4') do
+  erb(:page4)
+end
+
+post('/page4') do
   rope = Item.create({:description => "Rope"})
   @items = Item.all()
   answer1 = params.fetch('first_spot')
   answer2 = params.fetch('second_spot')
-  answer3 = params.fetch('third_spot')
-  if answer1 == "6" && answer2 == "push" && answer3 == "join"
-    erb(:page4)
+  if answer1 == "find" && answer2 == "total_potions"
+    erb(:page5)
   else
-    erb(:error)
+    @error = true
+    erb(:page4)
   end
 end
 
 get('/page5') do
+  erb(:page5)
+end
+
+post('/page5') do
   answer1 = params.fetch('first_spot')
   answer2 = params.fetch('second_spot')
-  if answer1 == "find" && answer2 == "total_potions"
+  if answer1 == "params" && answer2 == "key_id"
     # found_item = Item.find(params.fetch('4').to_i())
     # found_item.delete
-    erb(:page5)
+    erb(:page6)
   else
-    erb(:error)
+    @error = true
+    erb(:page5)
   end
 end
 
 get('/page6') do
-  answer1 = params.fetch('first_spot')
-  answer2 = params.fetch('second_spot')
-  if answer1 == "params" && answer2 == "key_id"
-    erb(:page6)
-  else
-    erb(:error)
-  end
+  erb(:page6)
 end
 
-post('/page6error') do
-    erb(:errordoor)
+post('/page6') do
+  @error = true
+  erb(:page6)
 end
 
 get('/page7') do
   erb(:page7)
 end
 
-get('/page8') do
+post('/page7') do
   answer1 = params.fetch('first_spot')
   answer2 = params.fetch('second_spot')
   if answer1 == "upcase" && answer2 == "end"
     erb(:page8)
   else
-    erb(:error)
+    @error = true
+    erb(:page7)
   end
 end
 
-# get('/page9') do
-#   answer1 = params.fetch('first_spot')
-#   answer2 = params.fetch('second_spot')
-#   answer3 = params.fetch('third_spot')
-#   if answer1 == "each" && answer2 == "shift" && answer3 == "part"
-#     erb(:page9)
-#   else
-#     erb(:error)
-#   end
-# end
+get('/page8') do
+  erb(:page8)
+end
+
+post('/page8') do
+  answer1 = params.fetch('first_spot')
+  answer2 = params.fetch('second_spot')
+  answer3 = params.fetch('third_spot')
+  if answer1 == "each" && answer2 == "shift" && answer3 == "part"
+    erb(:page9)
+  else
+    @error = true
+    erb(:page8)
+  end
+end
 
 get('/page9') do
   @error = false
